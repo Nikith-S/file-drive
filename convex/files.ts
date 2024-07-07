b735662f416e616722c131b6d0faa6f35fe0a34b
Nikith-S <nikith391@gmail.com>
2024-07-07 22:41:59 +0530

    usemutation and usequery is implemented

---
+++ convex/files.ts
@@ -0,0 +1,23 @@
+import {mutation, query} from './_generated/server'
+import {v} from 'convex/values'
+
+
+export const createFile = mutation({
+    args:{
+        name:v.string(),
+    },
+    async handler(ctx, args){
+        await ctx.db.insert("files", {
+            name: args.name
+        })
+
+    }
+})
+
+
+export const getFiles = query({
+    args: {},
+    async handler(ctx, args){
+        return ctx.db.query('files').collect()
+    }
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+"use client"; // Ensures this is a client component
+
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
+import { useMutation, useQueries, useQuery } from "convex/react";
 import Image from "next/image";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
+
+  const handleCreateFile = () => {
+    createFile({ name: "hello world" })
+      .then(() => {
+        console.log("File created successfully");
+      })
+      .catch((error) => {
+        console.error("Error creating file:", error);
+      });
+  };
+
   return (
     <div className="text-center">
       <SignedIn>
@@ -14,6 +31,12 @@ export default function Home() {
       </SignedOut>
 
       <Button>Hello world</Button>
+
+      <button onClick={handleCreateFile}>Click Me</button>
+
+      {files?.map((file) => (
+        <div key={file._id}>{file.name}</div>
+      ))}
     </div>
   );
 }
