74da1a5af1ab64a1f99ea025c59a0d6efec35317
Nikith-S <nikith391@gmail.com>
2024-07-08 21:34:39 +0530

    files changes made

--- convex/files.ts
+++ convex/files.ts
@@ -1,23 +1,34 @@
-import {mutation, query} from './_generated/server'
-import {v} from 'convex/values'
-
+import { mutation, query } from './_generated/server';
+import { ConvexError, v } from 'convex/values';
 
 export const createFile = mutation({
-    args:{
-        name:v.string(),
-    },
-    async handler(ctx, args){
-        await ctx.db.insert("files", {
-            name: args.name
-        })
-
+  args: {
+    name: v.string(),
+    orgId: v.string(),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new ConvexError("you need to be logged in");
     }
-})
 
+    await ctx.db.insert("files", {
+      name: args.name,
+      orgId: args.orgId,
+    });
+  },
+});
 
 export const getFiles = query({
-    args: {},
-    async handler(ctx, args){
-        return ctx.db.query('files').collect()
+  args: {
+    orgId: v.string(),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new ConvexError("you need to be logged in");
     }
-})
+
+    return ctx.db.query('files').withIndex('by_orgId', q => q.eq('orgId', args.orgId)); // Updated index name
+  },
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
 "use client"; // Ensures this is a client component
 
 import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, useOrganization } from "@clerk/nextjs";
 import { useMutation, useQueries, useQuery } from "convex/react";
 import Image from "next/image";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const {organization} = useOrganization();
+  organization?.id
   const createFile = useMutation(api.files.createFile);
-  const files = useQuery(api.files.getFiles);
+  const files = useQuery(api.files.getFiles, 
+    organization?.id ? {orgId :organization.id} : "skip"
+  );
 
   const handleCreateFile = () => {
-    createFile({ name: "hello world" })
+
+    if(!organization) return
+    createFile({ name: "hello world",
+      orgId:organization.id
+     })
       .then(() => {
         console.log("File created successfully");
       })
